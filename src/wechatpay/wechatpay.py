0bf66019f6c6dc03048c4ab719f13695065425e1
Haotong Chen <hereischen@gmail.com>
2015-09-21 16:57:08 +0800

    if config is not None

--- src/wechatpay/wechatpay.py
+++ src/wechatpay/wechatpay.py
@@ -27,7 +27,7 @@ def get_config(name, config_default=None):
     or django setting.py
     """
     config = os.environ.get(name, getattr(settings, name, config_default))
-    if config:
+    if config is not None:
         return config
     else:
         raise ImproperlyConfigured("Can't find config for '%s' either in environment"
